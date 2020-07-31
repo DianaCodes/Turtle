8bb89f6e22865ebeff8af8d42773b320af462b18
DianaCodes <DianaArita500@gmail.com>
2020-07-31 16:14:13 -0500

    Changes to editor page and dbConfig;

--- dbConfig.php
+++ dbConfig.php
@@ -1,4 +1,4 @@
 <?php
-$db = new mysqli('localhost','root','Twizzlers100','diana_database')
+$db = new mysqli('localhost','root','Twizzlers100!','diana_database')
 or die('Error connecting to MySQL server.');
 ?>
--- editor.php
+++ editor.php
@@ -134,8 +134,6 @@ include 'dbConfig.php';
     ?>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
-    <script type="text/javascript" src="ajaxform.js"></script>
     
     <script src="turtle.js"></script>
 
